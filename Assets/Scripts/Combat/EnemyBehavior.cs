dcf1a211c342bc668d7bb7f4eea689b28eb218f8
Jason Nguyen <jatn@uw.edu>
2023-07-03 09:55:52 -0700

    Comment out build issues

--- Assets/Scripts/Combat/EnemyBehavior.cs
+++ Assets/Scripts/Combat/EnemyBehavior.cs
@@ -12,7 +12,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 
 // consider making abstract
