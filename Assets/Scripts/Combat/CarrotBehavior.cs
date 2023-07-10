a1e9855c995f80ec774ecac778a26f4319ba7a85
unknown <jinochai@uw.edu>
2023-07-10 09:12:18 -0700

    added carrot enemy and placeholder for boss

---
+++ Assets/Scripts/Combat/CarrotBehavior.cs
@@ -0,0 +1,158 @@
+﻿/******************************************************************************
+ * Pumpkin behavior file. Inherits Enemy Behavior
+ * 
+ * Authors: Alicia T, Jason N, Jino C
+ *****************************************************************************/
+//#define Debug
+using NUnit.Framework.Internal;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Services.Lobbies.Models;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.LowLevel;
+using UnityEngine.UI;
+
+public class CarrotBehavior : EnemyBehavior
+{
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private bool isSpinning;
+    private bool idleLock;
+
+    // stuff needed for random move
+    private Vector2 dest;
+
+    // stuff needed for drill move
+    private Vector2 target;
+
+    private GameObject indInstance;
+
+    [SerializeField]
+    private float moveBias = 0.8f;
+
+    // indicator UI
+    [SerializeField]
+    private GameObject indicator = null;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        isSpinning = false;
+        idleLock = false;
+
+        dest = transform.position;
+
+        // have to be SUPER CAREFUL there are no exceptions during runtime, otherwise this WILL NOT run
+        InvokeRepeating("FindPlayer", 0.0f, 2.0f);
+    }
+
+    void Update()
+    {
+        Color c = GetComponent<Renderer>().material.color;
+        if (isSpinning)
+        {
+            c = new Color(c.r, c.g, c.b, c.a - (0.5f * Time.deltaTime));
+            GetComponent<Renderer>().material.color = c;
+        }
+        if (c.a <= 0)
+        {
+            GetComponent<Collider2D>().enabled = false;
+            IndicateServerRpc(target);
+            transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * getSpeed());
+            if (transform.position.x == target.x && transform.position.y == target.y)
+            {
+                DeindicateServerRpc();
+                c = new Color(c.r, c.g, c.b, 1.0f);
+                GetComponent<Renderer>().material.color = c;
+                GetComponent<Collider2D>().enabled = true;
+                idleLock = false;
+                isSpinning = false;
+                animator.ResetTrigger("spin");
+            }
+        } 
+        if (!idleLock) 
+        {
+            //Move();
+        }
+    }
+
+    public void Move()
+    {
+        float currX = transform.position.x;
+        float currY = transform.position.y;
+        if (currX == dest.x && currY == dest.y)
+        {
+            float randX = Random.Range(currX - moveBias, currX + moveBias);
+            float randY = Random.Range(currY - moveBias, currY + moveBias);
+
+            if (Random.Range(1, 1001) > 999)
+            {
+                if (randX - currX < 0)
+                {
+                    spriteRenderer.flipX = false;
+                }
+                else if (randX - currX > 0)
+                {
+                    spriteRenderer.flipX = true;
+                }
+                dest = new Vector2(randX, randY);
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, dest, Time.deltaTime * getSpeed());
+    }
+
+    // tests for nearest player in radius and locks on to them
+    public void FindPlayer()
+    {
+        if (!isSpinning)
+        {
+            if (GameObject.FindWithTag("Player") != null)
+            {
+                GameObject[] playerLoc = GameObject.FindGameObjectsWithTag("Player");
+                foreach (GameObject player in playerLoc)
+                {
+                    target = player.transform.position;
+                    if (Vector2.Distance(transform.position, target) < getAggroRange())
+                    {
+                        if (Vector2.Distance(transform.position, target) < getAggroRange())
+                        {
+                            if (target.x - transform.position.x < 0)
+                            {
+                                spriteRenderer.flipX = false;
+                            }
+                            else if (target.x - transform.position.x > 0)
+                            {
+                                spriteRenderer.flipX = true;
+                            }
+
+                            idleLock = true;
+                            isSpinning = true;
+                            animator.SetTrigger("spin");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    [ServerRpc]
+    public void IndicateServerRpc(Vector2 loc)
+    {
+        if (indInstance == null)
+        {
+            indInstance = Instantiate(indicator, loc, Quaternion.identity);
+            indInstance.GetComponent<NetworkObject>().Spawn(true);
+        }
+    }
+
+    [ServerRpc]
+    public void DeindicateServerRpc()
+    {
+        indInstance.GetComponent<NetworkObject>().Despawn(true);
+        Destroy(indInstance);
+        indInstance = null;
+    }
+}
