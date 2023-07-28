6b458c51f943b3fd984d2d99c3d32144a0c2a603
Jason <jatnguyen0800@Yahoo.com>
2023-07-27 23:42:16 -0700

    Adjust overworld spawn scripts

--- Assets/Scripts/Overworld/OverworldSpawner.cs
+++ Assets/Scripts/Overworld/OverworldSpawner.cs
@@ -11,50 +11,89 @@ using UnityEngine.UI;
 
 public class OverworldSpawner : NetworkBehaviour
 {
-
     public List<GameObject> structure;
+    public GameObject spawnPoint;
 
-    public GameObject bounds;
-
-    // Allows to set number of objects to spawn in world
+    // Allows to set the number of objects to spawn in the world
     [SerializeField]
     private int numOfSpawns = 0;
 
-    void Start() {
-        spawnObject();
+    void Start()
+    {
+        SpawnObjects();
     }
 
-    // public void Spawn()
-    // {
-    //     StartCoroutine(spawnObject(4.0f, structure));
-    //     StartCoroutine(spawnObject(4.0f, structure2));
-    // }
-
-    // private IEnumerator spawnObject(float interval, GameObject s) {
-    //     yield return new WaitForSeconds(interval);
-    //     GameObject newObject = Instantiate(s, new Vector3(Random.Range(-10f, 10), Random.Range(-10f, 10f), 0), Quaternion.identity);
-    //     newObject.GetComponent<NetworkObject>().Spawn();
-    //     StartCoroutine(spawnObject(4.0f, s));
-    // }
-
-    public void spawnObject()
+    public void SpawnObjects()
     {
-        MeshCollider c = bounds.GetComponent<MeshCollider>();
-        float screenX, screenY;
+        // Use UnityEngine.Bounds instead of custom Bounds
+        UnityEngine.Bounds cBounds = spawnPoint.GetComponent<MeshCollider>().bounds;
         GameObject spawn;
-        Vector2 pos;
-        for (int i = 0; i < numOfSpawns; i++) {
+
+        // Create a list to store the positions of already spawned objects
+        List<Vector2> spawnedPositions = new List<Vector2>();
+
+        for (int i = 0; i < numOfSpawns; i++)
+        {
             int rand = Random.Range(0, structure.Count);
             spawn = structure[rand];
-            screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
-            screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
-            pos = new Vector2(screenX, screenY);
 
-            Instantiate(spawn, pos, spawn.transform.rotation);
+            // Attempt to find a non-overlapping position
+            Vector2 randomPosition = FindNonOverlappingPosition(cBounds, spawn.transform.localScale, spawnedPositions);
 
+            // Instantiate the object if a suitable position is found
+            if (randomPosition != Vector2.zero)
+            {
+                Instantiate(spawn, randomPosition, spawn.transform.rotation);
+                spawnedPositions.Add(randomPosition); // Add the position to the list of spawned positions
+            }
         }
     }
 
+    private Vector2 FindNonOverlappingPosition(UnityEngine.Bounds bounds, Vector2 objectSize, List<Vector2> spawnedPositions)
+    {
+        // Define the number of attempts to find a non-overlapping position
+        int maxAttempts = 100;
+        int attempts = 0;
+
+        while (attempts < maxAttempts)
+        {
+            // Generate a random position within the bounds
+            float screenX = Random.Range(bounds.min.x, bounds.max.x);
+            float screenY = Random.Range(bounds.min.y, bounds.max.y);
+            Vector2 randomPosition = new Vector2(screenX, screenY);
+
+            // Check if the object overlaps with any other existing objects
+            if (!IsOverlappingOtherObjects(randomPosition, objectSize, spawnedPositions))
+            {
+                return randomPosition; // Found a non-overlapping position
+            }
+
+            attempts++;
+        }
+
+        // Return Vector2.zero if no non-overlapping position is found after maxAttempts
+        return Vector2.zero;
+    }
+
+    private bool IsOverlappingOtherObjects(Vector2 position, Vector2 objectSize, List<Vector2> spawnedPositions)
+    {
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(position, objectSize, 0f);
+
+        // Check if any colliders were found
+        if (colliders.Length > 0)
+        {
+            // Check if any of the colliders belong to the spawned objects
+            foreach (var collider in colliders)
+            {
+                if (spawnedPositions.Contains(collider.transform.position))
+                {
+                    return true; // Overlapping with a spawned object
+                }
+            }
+        }
+
+        return false; // No overlapping with spawned objects
+    }
 
     private void DestroyObjects()
     {
@@ -64,3 +103,4 @@ public class OverworldSpawner : NetworkBehaviour
         }
     }
 }
+
